5134e2c3787e4164f65af4b5a2509eecf1f07488
zomlaaa95 <zomlaaa95@gmail.com>
2017-09-16 15:35:48 +0200

    fixed database create and insert to lower keys

--- database/eBookRepositoryInsertScript.sql
+++ database/eBookRepositoryInsertScript.sql
@@ -2,73 +2,73 @@ create database ebookrepository
 
 Use ebookrepository
 
-Insert Into Book_Language(language_name, language_deleted) Values ('American', 0);
-Insert Into Book_Language(language_name, language_deleted) Values ('French', 0);
-Insert Into Book_Language(language_name, language_deleted) Values ('Serbian', 0);
+Insert Into book_language(language_name, language_deleted) Values ('American', 0);
+Insert Into book_language(language_name, language_deleted) Values ('French', 0);
+Insert Into book_language(language_name, language_deleted) Values ('Serbian', 0);
 
-Insert Into Category(category_name, category_deleted) Values ('Biography', 0);
-Insert Into Category(category_name, category_deleted) Values ('Romance', 0);
-Insert Into Category(category_name, category_deleted) Values ('Mistery', 0);
-Insert Into Category(category_name, category_deleted) Values ('Horror', 0);
-Insert Into Category(category_name, category_deleted) Values ('Business', 0);
+Insert Into category(category_name, category_deleted) Values ('Biography', 0);
+Insert Into category(category_name, category_deleted) Values ('Romance', 0);
+Insert Into category(category_name, category_deleted) Values ('Mistery', 0);
+Insert Into category(category_name, category_deleted) Values ('Horror', 0);
+Insert Into category(category_name, category_deleted) Values ('Business', 0);
 
 # book files
 # .pdf = application/pdf
 
 # Biography
-Insert Into Book_File(file_name, file_mime) Values ('abrahamLincoln1', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('miracleBoy2', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('theAutobiographyOfCharlesDarwin3', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('abrahamLincoln1', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('miracleBoy2', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('theAutobiographyOfCharlesDarwin3', 'application/pdf');
 
 # Romance
-Insert Into Book_File(file_name, file_mime) Values ('attractions13', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('loveLetters14', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('pridePrejudice15', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('attractions13', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('loveLetters14', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('pridePrejudice15', 'application/pdf');
 
 # Mistery
-Insert Into Book_File(file_name, file_mime) Values ('adventuresSherlockHolmes10', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('darkRemnants11', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('handcuffsAndHighHeels12', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('adventuresSherlockHolmes10', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('darkRemnants11', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('handcuffsAndHighHeels12', 'application/pdf');
 
 # Horror
-Insert Into Book_File(file_name, file_mime) Values ('bedtime7', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('dracula8', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('theDeadlyOnez9', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('bedtime7', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('dracula8', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('theDeadlyOnez9', 'application/pdf');
 
 # Business
-Insert Into Book_File(file_name, file_mime) Values ('humanResourcesBestPracticesGuide4', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('recruit5', 'application/pdf');
-Insert Into Book_File(file_name, file_mime) Values ('topTenSellingTheLumberjackChronicles6', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('humanResourcesBestPracticesGuide4', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('recruit5', 'application/pdf');
+Insert Into book_file(file_name, file_mime) Values ('topTenSellingTheLumberjackChronicles6', 'application/pdf');
 
 
-Insert Into App_User(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Milan', 'Antic', 'milan', 'antic', null, 'administrator', 0);
-Insert Into App_User(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Nikola', 'Pavlovic', 'nikola', 'pavlovic', 1, 'subscriber', 0);
-Insert Into App_User(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Stefan', 'Simic', 'stefan', 'simic', 2, 'subscriber', 0);
-Insert Into App_User(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Novak', 'Music', 'novak', 'music', 3, 'subscriber', 0);
-Insert Into App_User(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Miroslav', 'Tomic', 'miroslav', 'tomic', null, 'subscriber', 0);
-Insert Into App_User(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Jovan', 'Vojinovic', 'jovan', 'vojinovic', null, 'subscriber', 0);
+Insert Into app_user(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Milan', 'Antic', 'milan', 'antic', null, 'administrator', 0);
+Insert Into app_user(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Nikola', 'Pavlovic', 'nikola', 'pavlovic', 1, 'subscriber', 0);
+Insert Into app_user(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Stefan', 'Simic', 'stefan', 'simic', 2, 'subscriber', 0);
+Insert Into app_user(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Novak', 'Music', 'novak', 'music', 3, 'subscriber', 0);
+Insert Into app_user(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Miroslav', 'Tomic', 'miroslav', 'tomic', null, 'subscriber', 0);
+Insert Into app_user(app_user_firstname, app_user_lastname, app_user_username, app_user_password, app_user_category_id, app_user_type, app_user_deleted) Values ('Jovan', 'Vojinovic', 'jovan', 'vojinovic', null, 'subscriber', 0);
 
 # Biography
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('ABRAHAM LINCOLN', 'JAMES RUSSELL LOWELL', 'biography|president|justice|assasination', 1865, 1, 1, 1, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Miracle Boy', 'Olatunji Joan Olufolake|Olatunji Ayodele Oluwaseyi', 'biography|miracle|africa|nigeria|war', 2013, 1, 1, 2, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Autobiography of Charles Darwin', 'Charles Darwin', 'biography|science|biology', 1887, 1, 1, 3, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('ABRAHAM LINCOLN', 'JAMES RUSSELL LOWELL', 'biography|president|justice|assasination', 1865, 1, 1, 1, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Miracle Boy', 'Olatunji Joan Olufolake|Olatunji Ayodele Oluwaseyi', 'biography|miracle|africa|nigeria|war', 2013, 1, 1, 2, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Autobiography of Charles Darwin', 'Charles Darwin', 'biography|science|biology', 1887, 1, 1, 3, 0);
 
 # Romance
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Attractions', 'Joshua Edward Smith', 'romance|love|drama', 2016, 1, 2, 4, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Love Letters', 'Bryan Mooney', 'romance|love|woman|complicated|relationship', 2011, 1, 2, 5, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Pride and Prejudice', 'Jane Austen', 'romance|old|middle age|castle|letter|love', 1813, 1, 2, 6, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Attractions', 'Joshua Edward Smith', 'romance|love|drama', 2016, 1, 2, 4, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Love Letters', 'Bryan Mooney', 'romance|love|woman|complicated|relationship', 2011, 1, 2, 5, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Pride and Prejudice', 'Jane Austen', 'romance|old|middle age|castle|letter|love', 1813, 1, 2, 6, 0);
 
 # Mistery
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Adventures Of Sherlock Holmes', 'Arthur Conan Doyle', 'mistery|crime|england|detective', 2012, 1, 3, 7, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Dark Remnants - Street Games: Book One', 'L.K.Hill', 'mistery|crime|darkness|assasination|murder', 2014, 1, 3, 8, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Handcuffs & High Heels', 'J.M.Edwards', 'mistery|woman|crime|detective|murder', 2014, 1, 3, 9, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Adventures Of Sherlock Holmes', 'Arthur Conan Doyle', 'mistery|crime|england|detective', 2012, 1, 3, 7, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Dark Remnants - Street Games: Book One', 'L.K.Hill', 'mistery|crime|darkness|assasination|murder', 2014, 1, 3, 8, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Handcuffs & High Heels', 'J.M.Edwards', 'mistery|woman|crime|detective|murder', 2014, 1, 3, 9, 0);
 
 # Horror
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Bedtime and other tales of terror', 'Michael Whitehouse', 'horror|bed|terror|darkness', 2013, 1, 4, 10, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Dracula', 'Bram Stoker|Heidi Klum', 'horror|romania|vampire|fangs|blood|castle', 1897, 1, 4, 11, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Deadly Onez', 'Ziad Antar', 'horror|walking dead|zombie|terror|cemetery', 2012, 1, 4, 12, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Bedtime and other tales of terror', 'Michael Whitehouse', 'horror|bed|terror|darkness', 2013, 1, 4, 10, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Dracula', 'Bram Stoker|Heidi Klum', 'horror|romania|vampire|fangs|blood|castle', 1897, 1, 4, 11, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Deadly Onez', 'Ziad Antar', 'horror|walking dead|zombie|terror|cemetery', 2012, 1, 4, 12, 0);
 
 # Business
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('HUMAN RESOURCES BEST PRACTICES GUIDE', 'Solomon Hill', 'business|money|stock|market|staff', 2010, 1, 5, 13, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Recruit', 'DAVID SWEET', 'business|money|finance|recruitment|staff', 2007, 1, 5, 14, 0);
-Insert Into eBook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Lumberjack Chronicles', 'DAN NORMAN', 'business|money|finance|chronicle', 2007, 1, 5, 15, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('HUMAN RESOURCES BEST PRACTICES GUIDE', 'Solomon Hill', 'business|money|stock|market|staff', 2010, 1, 5, 13, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('Recruit', 'DAVID SWEET', 'business|money|finance|recruitment|staff', 2007, 1, 5, 14, 0);
+Insert Into ebook(eBook_title, eBook_author, eBook_keywords, eBook_publication_year, eBook_language, eBook_category, eBook_file_id, eBook_deleted) Values ('The Lumberjack Chronicles', 'DAN NORMAN', 'business|money|finance|chronicle', 2007, 1, 5, 15, 0);
--- database/eBookRepositoryScript.sql
+++ database/eBookRepositoryScript.sql
@@ -1,29 +1,29 @@
-Create Database eBookRepository
+Create Database ebookrepository
 
-Use eBookRepository
+Use ebookrepository
 
-Create Table Book_Language
+Create Table book_language
 (
 	language_id Integer Primary Key Not Null Auto_increment,
 	language_name Varchar(30),
     language_deleted Bit
 );
 
-Create Table Category
+Create Table category
 (
 	category_id Integer Primary Key Not Null Auto_increment,
 	category_name Varchar(30),
     category_deleted Bit
 );
 
-Create Table Book_File
+Create Table book_file
 (
 	file_id Integer Primary Key Not Null Auto_increment,
     file_name Varchar(200),
     file_mime Varchar(100)
 );
 
-Create Table App_User
+Create Table app_user
 (
 	app_user_id Integer Primary Key Not Null Auto_increment,
 	app_user_firstname Varchar(30),
@@ -34,10 +34,10 @@ Create Table App_User
     app_user_type Varchar(30),
     app_user_deleted Bit,
     
-    Foreign Key (app_user_category_id) References Category(category_id)
+    Foreign Key (app_user_category_id) References category(category_id)
 );
 
-Create Table eBook
+Create Table ebook
 (
 	eBook_id Integer Primary Key Not Null Auto_increment,
     eBook_title Varchar(80),
@@ -49,7 +49,7 @@ Create Table eBook
     eBook_file_id Integer,
     eBook_deleted Bit,
     
-    Foreign Key (eBook_language) References Book_Language(language_id),
-    Foreign Key (eBook_category) References Category(category_id),
-    Foreign Key (eBook_file_id) References Book_File(file_id)
+    Foreign Key (eBook_language) References book_language(language_id),
+    Foreign Key (eBook_category) References category(category_id),
+    Foreign Key (eBook_file_id) References book_file(file_id)
 );
